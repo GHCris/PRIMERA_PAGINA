ef508b50fcc47d8e8509608b48f0f7dd00ba090a
GHCris <clgrisolia@gmail.com>
2023-07-07 17:27:35 -0300

    first_commit

---
+++ hospedaje/views.py
@@ -0,0 +1,13 @@
+from django.views.generic import TemplateView
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+class BarilochePage(TemplateView):
+    template_name = "bariloche.html"
+
+class CarlosPazPage(TemplateView):
+    template_name = "carlospaz.html"
+
+class SaltaPage(TemplateView):
+    template_name = "salta.html"    
